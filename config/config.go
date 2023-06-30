ea5ce00e6728901fa5e49798c4a57b02d2ee2ede
gabe <gcohen@squareup.com>
2023-06-30 11:53:17 -0700

    update config

--- config/config.go
+++ config/config.go
@@ -78,7 +78,7 @@ func GetDefaultConfig() Config {
 			Topic:                 "diddht",
 			LocalDiscovery:        true,
 			ResolverEndpoint:      "https://dev.uniresolver.io/",
-			BootstrapPeers:        []string{"/ip4/54.226.19.143/tcp/8503/p2p/12D3KooWMmTajXNqysKJhR8LaED1cVjzz7RsaRgchHqzs3CZ1CMb"},
+			BootstrapPeers:        []string{"/ip4/54.226.19.143/tcp/8503/p2p/12D3KooWJgfQ14jcrjXD1VusChcdZxKsSXrhk4xDDtgWbEYGYe5A"},
 			EnforceSignedMessages: false,
 		},
 	}
