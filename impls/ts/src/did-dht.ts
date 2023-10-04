b41b370f5e7ce3df810a2ce32792a5233f6bab97
gabe <gcohen@squareup.com>
2023-10-04 13:17:28 -0700

    did tests

--- impls/ts/src/did-dht.ts
+++ impls/ts/src/did-dht.ts
@@ -1,18 +1,18 @@
-import type {PrivateKeyJwk, PublicKeyJwk, JwkKeyPair} from '@web5/crypto';
-import type {
-  DidDocument,
-  DidKeySetVerificationMethodKey,
-  DidMethod,
-  DidResolutionResult,
-  DidService,
-  VerificationMethod
-} from '@web5/dids';
+import type {JwkKeyPair, PublicKeyJwk} from '@web5/crypto';
 import {EcdsaAlgorithm, EdDsaAlgorithm, Jose, Web5Crypto} from '@web5/crypto';
+import type {
+    DidDocument,
+    DidKeySetVerificationMethodKey,
+    DidMethod,
+    DidResolutionResult,
+    DidService,
+    VerificationMethod
+} from '@web5/dids';
 import z32 from 'z32';
 
 const SupportedCryptoAlgorithms = [
-  'Ed25519',
-  'secp256k1'
+    'Ed25519',
+    'secp256k1'
 ] as const;
 
 export type DidDhtCreateOptions = {
@@ -28,155 +28,164 @@ export type DidDhtKeySet = {
 
 export class DidDhtMethod implements DidMethod {
 
-  public static methodName = 'dht';
+    public static methodName = 'dht';
 
-  public static async create(options?: DidDhtCreateOptions): Promise<{ did: DidDocument, keySet: DidDhtKeySet }> {
-    let {publish, keySet, services} = options ?? {};
+    public static async create(options?: DidDhtCreateOptions): Promise<{ did: DidDocument, keySet: DidDhtKeySet }> {
+        let {publish, keySet, services} = options ?? {};
 
-    // Begin constructing a PortableDid
-    const did: Partial<DidDocument> = {};
+        // Begin constructing a PortableDid
+        const did: Partial<DidDocument> = {};
 
-    // Generate missing keys if not provided in the options
-    keySet = await this.generateKeySet({keySet});
+        // Generate missing keys if not provided in the options
+        keySet = await this.generateKeySet({keySet});
 
-    // Get the identifier and set it
-    const id = await this.getDidIdentifier({key: keySet.identityKey.publicKeyJwk});
-    did.id = id;
+        // Get the identifier and set it
+        const id = await this.getDidIdentifier({key: keySet.identityKey.publicKeyJwk});
+        did.id = id;
 
-    // add identity key to the verificationMethod and relationship arrays
-    const identityKey: Partial<VerificationMethod> = {
-      id           : `${id}#0`,
-      type         : 'JsonWebKey2020',
-      controller   : id,
-      publicKeyJwk : keySet.identityKey.publicKeyJwk
-    };
-    did.verificationMethod = [identityKey as VerificationMethod];
+        // add identity key to the verificationMethod and relationship arrays
+        const identityKey: Partial<VerificationMethod> = {
+            id: `${id}#0`,
+            type: 'JsonWebKey2020',
+            controller: id,
+            publicKeyJwk: keySet.identityKey.publicKeyJwk
+        };
+        did.verificationMethod = [identityKey as VerificationMethod];
 
-    // add all other keys to the verificationMethod and relationship arrays
-    const relationshipsMap: { [key: string]: string[] } = {
-      authentication       : ['#0'],
-      assertionMethod      : ['#0'],
-      capabilityInvocation : ['#0'],
-      capabilityDelegation : ['#0'],
-      keyAgreement         : []
-    };
+        // add all other keys to the verificationMethod and relationship arrays
+        const relationshipsMap: { [key: string]: string[] } = {
+            authentication: ['#0'],
+            assertionMethod: ['#0'],
+            capabilityInvocation: ['#0'],
+            capabilityDelegation: ['#0'],
+            keyAgreement: []
+        };
 
-    for (const key of keySet.verificationMethodKeys) {
-      const vm: Partial<VerificationMethod> = {
-        id           : `${id}#${key.publicKeyJwk.kid}`,
-        type         : 'JsonWebKey2020',
-        controller   : id,
-        publicKeyJwk : key.publicKeyJwk
-      };
+        for (const key of keySet.verificationMethodKeys) {
+            const vm: Partial<VerificationMethod> = {
+                id: `${id}#${key.publicKeyJwk.kid}`,
+                type: 'JsonWebKey2020',
+                controller: id,
+                publicKeyJwk: key.publicKeyJwk
+            };
 
-      for (const relationship of key.relationships) {
-        if (relationshipsMap[relationship]) {
-          relationshipsMap[relationship].push(key.publicKeyJwk.kid);
+            for (const relationship of key.relationships) {
+                if (relationshipsMap[relationship]) {
+                    relationshipsMap[relationship].push(key.publicKeyJwk.kid);
+                }
+            }
+
+            did.verificationMethod.push(vm as VerificationMethod);
         }
-      }
 
-      did.verificationMethod.push(vm as VerificationMethod);
+        // add all services
+        if (services) {
+            did.service = services;
+        }
+        return {did: did as DidDocument, keySet: keySet};
     }
 
-    // add all services
-    if (services) {
-      did.service = services;
+    public static async publish(key: DidDhtKeySet, didDocument: DidDocument): Promise<DidResolutionResult | undefined> {
+        throw new Error('Method not implemented.');
     }
-    return {did: did as DidDocument, keySet: keySet};
-  }
 
-  public static async publish(key: DidDhtKeySet, didDocument: DidDocument): Promise<DidResolutionResult | undefined> {
-    throw new Error('Method not implemented.');
-  }
+    public static async resolve(did: string): Promise<DidDocument> {
+        throw new Error('Method not implemented.');
+    }
 
-  public static async resolve(did: string): Promise<DidDocument> {
-    throw new Error('Method not implemented.');
-  }
-
-  public static async getDidIdentifier(options: {
+    public static async getDidIdentifier(options: {
         key: PublicKeyJwk
     }): Promise<string> {
-    const {key} = options;
+        const {key} = options;
 
-    const cryptoKey = await Jose.jwkToCryptoKey({key});
-    const identifier = z32.encode(cryptoKey.material);
-    return 'did:dht:' + identifier;
-  }
+        const cryptoKey = await Jose.jwkToCryptoKey({key});
+        const identifier = z32.encode(cryptoKey.material);
+        return 'did:dht:' + identifier;
+    }
 
-  public static async generateJwkKeyPair(options: {
+    public static async generateJwkKeyPair(options: {
         keyAlgorithm: typeof SupportedCryptoAlgorithms[number],
         keyId?: string
     }): Promise<JwkKeyPair> {
-    const {keyAlgorithm, keyId} = options;
+        const {keyAlgorithm, keyId} = options;
 
-    let cryptoKeyPair: Web5Crypto.CryptoKeyPair;
+        let cryptoKeyPair: Web5Crypto.CryptoKeyPair;
 
-    switch (keyAlgorithm) {
-      case 'Ed25519': {
-        cryptoKeyPair = await new EdDsaAlgorithm().generateKey({
-          algorithm   : {name: 'EdDSA', namedCurve: 'Ed25519'},
-          extractable : true,
-          keyUsages   : ['sign', 'verify']
-        });
-        break;
-      }
+        switch (keyAlgorithm) {
+            case 'Ed25519': {
+                cryptoKeyPair = await new EdDsaAlgorithm().generateKey({
+                    algorithm: {name: 'EdDSA', namedCurve: 'Ed25519'},
+                    extractable: true,
+                    keyUsages: ['sign', 'verify']
+                });
+                break;
+            }
 
-      case 'secp256k1': {
-        cryptoKeyPair = await new EcdsaAlgorithm().generateKey({
-          algorithm   : {name: 'ECDSA', namedCurve: 'secp256k1'},
-          extractable : true,
-          keyUsages   : ['sign', 'verify']
-        });
-        break;
-      }
+            case 'secp256k1': {
+                cryptoKeyPair = await new EcdsaAlgorithm().generateKey({
+                    algorithm: {name: 'ECDSA', namedCurve: 'secp256k1'},
+                    extractable: true,
+                    keyUsages: ['sign', 'verify']
+                });
+                break;
+            }
 
-      default: {
-        throw new Error(`Unsupported crypto algorithm: '${keyAlgorithm}'`);
-      }
+            default: {
+                throw new Error(`Unsupported crypto algorithm: '${keyAlgorithm}'`);
+            }
+        }
+
+        // Convert the CryptoKeyPair to JwkKeyPair.
+        const jwkKeyPair = await Jose.cryptoKeyToJwkPair({keyPair: cryptoKeyPair});
+
+        // Set kid values.
+        if (keyId) {
+            jwkKeyPair.privateKeyJwk.kid = keyId;
+            jwkKeyPair.publicKeyJwk.kid = keyId;
+        } else {
+            // If a key ID is not specified, generate RFC 7638 JWK thumbprint.
+            const jwkThumbprint = await Jose.jwkThumbprint({key: jwkKeyPair.publicKeyJwk});
+            jwkKeyPair.privateKeyJwk.kid = jwkThumbprint;
+            jwkKeyPair.publicKeyJwk.kid = jwkThumbprint;
+        }
+
+        return jwkKeyPair;
     }
 
-    // Convert the CryptoKeyPair to JwkKeyPair.
-    const jwkKeyPair = await Jose.cryptoKeyToJwkPair({keyPair: cryptoKeyPair});
-
-    // Set kid values.
-    if (keyId) {
-      jwkKeyPair.privateKeyJwk.kid = keyId;
-      jwkKeyPair.publicKeyJwk.kid = keyId;
-    } else {
-      // If a key ID is not specified, generate RFC 7638 JWK thumbprint.
-      const jwkThumbprint = await Jose.jwkThumbprint({key: jwkKeyPair.publicKeyJwk});
-      jwkKeyPair.privateKeyJwk.kid = jwkThumbprint;
-      jwkKeyPair.publicKeyJwk.kid = jwkThumbprint;
-    }
-
-    return jwkKeyPair;
-  }
-
-  public static async generateKeySet(options?: {
+    public static async generateKeySet(options?: {
         keySet?: DidDhtKeySet
     }): Promise<DidDhtKeySet> {
-    let {keySet = {}} = options ?? {};
+        let {keySet = {}} = options ?? {};
 
-    if (!keySet.identityKey) {
-      const identityKeyPair = await this.generateJwkKeyPair({
-        keyAlgorithm : 'Ed25519',
-        keyId        : '0'
-      });
-      keySet.identityKey = identityKeyPair;
-      keySet.verificationMethodKeys = [{
-        ...identityKeyPair,
-        relationships: ['authentication', 'assertionMethod', 'capabilityInvocation', 'capabilityDelegation']
-      }];
+        if (!keySet.identityKey) {
+            keySet.identityKey = await this.generateJwkKeyPair({
+                keyAlgorithm: 'Ed25519',
+                keyId: '0'
+            });
+        } else if (keySet.identityKey.publicKeyJwk.kid !== '0') {
+            throw new Error('The identity key must have a kid of 0');
+        }
+
+        const identityKeySetVerificationMethod: DidKeySetVerificationMethodKey = {
+            ...keySet.identityKey,
+            relationships: ['authentication', 'assertionMethod', 'capabilityInvocation', 'capabilityDelegation']
+        }
+
+        // add verificationMethodKeys for the identity key
+        if (keySet.verificationMethodKeys) {
+            keySet.verificationMethodKeys.push(identityKeySetVerificationMethod);
+        } else {
+            keySet.verificationMethodKeys = [identityKeySetVerificationMethod];
+        }
+
+        // Generate RFC 7638 JWK thumbprints if `kid` is missing from any key.
+        if (keySet.verificationMethodKeys) {
+            for (const key of keySet.verificationMethodKeys) {
+                if (key.publicKeyJwk) key.publicKeyJwk.kid ??= await Jose.jwkThumbprint({key: key.publicKeyJwk});
+                if (key.privateKeyJwk) key.privateKeyJwk.kid ??= await Jose.jwkThumbprint({key: key.privateKeyJwk});
+            }
+        }
+
+        return keySet;
     }
-
-    // Generate RFC 7638 JWK thumbprints if `kid` is missing from any key.
-    if (keySet.verificationMethodKeys) {
-      for (const key of keySet.verificationMethodKeys) {
-        if (key.publicKeyJwk) key.publicKeyJwk.kid ??= await Jose.jwkThumbprint({key: key.publicKeyJwk});
-        if (key.privateKeyJwk) key.privateKeyJwk.kid ??= await Jose.jwkThumbprint({key: key.privateKeyJwk});
-      }
-    }
-
-    return keySet;
-  }
 }
--- impls/ts/tests/did-dht.spec.ts
+++ impls/ts/tests/did-dht.spec.ts
@@ -1,49 +1,95 @@
+import chai from 'chai';
 import {expect} from 'chai';
+import chaiAsPromised from 'chai-as-promised';
 import {DidDhtMethod} from '../src/did-dht.js';
+import {DidKeySetVerificationMethodKey} from "@web5/dids";
+
+chai.use(chaiAsPromised);
 
 describe('did-dht', async () => {
-  describe('keypairs', async () => {
-    it('should generate a keypair', async () => {
-      const ed25519KeyPair = await DidDhtMethod.generateJwkKeyPair({keyAlgorithm: 'Ed25519'});
-      expect(ed25519KeyPair).to.exist;
-      expect(ed25519KeyPair).to.have.property('privateKeyJwk');
-      expect(ed25519KeyPair).to.have.property('publicKeyJwk');
-      expect(ed25519KeyPair.publicKeyJwk.kid).to.exist;
-      expect(ed25519KeyPair.publicKeyJwk.alg).to.equal('EdDSA');
-      expect(ed25519KeyPair.publicKeyJwk.kty).to.equal('OKP');
+    describe('keypairs', async () => {
+        it('should generate a keypair', async () => {
+            const ed25519KeyPair = await DidDhtMethod.generateJwkKeyPair({keyAlgorithm: 'Ed25519'});
+            expect(ed25519KeyPair).to.exist;
+            expect(ed25519KeyPair).to.have.property('privateKeyJwk');
+            expect(ed25519KeyPair).to.have.property('publicKeyJwk');
+            expect(ed25519KeyPair.publicKeyJwk.kid).to.exist;
+            expect(ed25519KeyPair.publicKeyJwk.alg).to.equal('EdDSA');
+            expect(ed25519KeyPair.publicKeyJwk.kty).to.equal('OKP');
 
-      const secp256k1KeyPair = await DidDhtMethod.generateJwkKeyPair({keyAlgorithm: 'secp256k1'});
-      expect(secp256k1KeyPair).to.exist;
-      expect(secp256k1KeyPair).to.have.property('privateKeyJwk');
-      expect(secp256k1KeyPair).to.have.property('publicKeyJwk');
-      expect(secp256k1KeyPair.publicKeyJwk.kid).to.exist;
-      expect(secp256k1KeyPair.publicKeyJwk.alg).to.equal('ES256K');
-      expect(secp256k1KeyPair.publicKeyJwk.kty).to.equal('EC');
-    });
-  });
-
-  describe('keysets', async () => {
-    it('should generate a keyset with no keyset passed in', async () => {
-        const keySet = await DidDhtMethod.generateKeySet();
-        expect(keySet).to.exist;
-        expect(keySet).to.have.property('identityKey');
-        expect(keySet).to.not.have.property('recoveryKey');
-        expect(keySet).to.not.have.property('updateKey');
-        expect(keySet).to.not.have.property('signingKey');
+            const secp256k1KeyPair = await DidDhtMethod.generateJwkKeyPair({keyAlgorithm: 'secp256k1'});
+            expect(secp256k1KeyPair).to.exist;
+            expect(secp256k1KeyPair).to.have.property('privateKeyJwk');
+            expect(secp256k1KeyPair).to.have.property('publicKeyJwk');
+            expect(secp256k1KeyPair.publicKeyJwk.kid).to.exist;
+            expect(secp256k1KeyPair.publicKeyJwk.alg).to.equal('ES256K');
+            expect(secp256k1KeyPair.publicKeyJwk.kty).to.equal('EC');
+        });
     });
 
-    it('should generate a keyset with a keyset passed in', async () => {
+    describe('keysets', async () => {
+        it('should generate a keyset with no keyset passed in', async () => {
+            const keySet = await DidDhtMethod.generateKeySet();
+            expect(keySet).to.exist;
+            expect(keySet).to.have.property('identityKey');
+            expect(keySet).to.have.property('verificationMethodKeys');
+            expect(keySet).to.not.have.property('recoveryKey');
+            expect(keySet).to.not.have.property('updateKey');
+            expect(keySet).to.not.have.property('signingKey');
+            expect(keySet.verificationMethodKeys).to.have.lengthOf(1);
+            expect(keySet.verificationMethodKeys[0].publicKeyJwk.kid).to.equal('0');
+        });
 
+        it('should generate a keyset with an identity keyset passed in (wrong kid)', async () => {
+            const ed25519KeyPair = await DidDhtMethod.generateJwkKeyPair({keyAlgorithm: 'Ed25519'});
+            expect(DidDhtMethod.generateKeySet({keySet: {identityKey: ed25519KeyPair}})).to.be.rejectedWith('The identity key must have a kid of 0');
+        });
+
+        it('should generate a keyset with an identity keyset passed in (correct kid)', async () => {
+            const ed25519KeyPair = await DidDhtMethod.generateJwkKeyPair({keyId: '0', keyAlgorithm: 'Ed25519'});
+            const keySet = await DidDhtMethod.generateKeySet({keySet: {identityKey: ed25519KeyPair}});
+            expect(keySet).to.exist;
+            expect(keySet).to.have.property('identityKey');
+            expect(keySet).to.have.property('verificationMethodKeys');
+            expect(keySet).to.not.have.property('recoveryKey');
+            expect(keySet).to.not.have.property('updateKey');
+            expect(keySet).to.not.have.property('signingKey');
+            expect(keySet.verificationMethodKeys).to.have.lengthOf(1);
+            expect(keySet.verificationMethodKeys[0].publicKeyJwk.kid).to.equal('0');
+        });
+
+        it('should generate a keyset with a non identity keyset passed in', async () => {
+            const ed25519KeyPair = await DidDhtMethod.generateJwkKeyPair({keyAlgorithm: 'Ed25519'});
+            const vm: DidKeySetVerificationMethodKey = {
+                publicKeyJwk: ed25519KeyPair.publicKeyJwk,
+                privateKeyJwk: ed25519KeyPair.privateKeyJwk,
+                relationships: ['authentication', 'assertionMethod', 'capabilityInvocation', 'capabilityDelegation']
+            }
+
+            const keySet = await DidDhtMethod.generateKeySet({keySet: {verificationMethodKeys: [vm]}});
+            expect(keySet).to.exist;
+            expect(keySet).to.have.property('identityKey');
+            expect(keySet).to.have.property('verificationMethodKeys');
+            expect(keySet).to.not.have.property('recoveryKey');
+            expect(keySet).to.not.have.property('updateKey');
+            expect(keySet).to.not.have.property('signingKey');
+            expect(keySet.verificationMethodKeys).to.have.lengthOf(2);
+
+            if (keySet.verificationMethodKeys[0].publicKeyJwk.kid === '0') {
+                expect(keySet.verificationMethodKeys[1].publicKeyJwk.kid).to.not.equal('0');
+            } else {
+                expect(keySet.verificationMethodKeys[1].publicKeyJwk.kid).to.equal('0');
+            }
+        });
     });
-  });
 
-  describe('dids', async () => {
-    it('should generate a did identifier', async () => {
+    describe('dids', async () => {
+        it('should generate a did identifier', async () => {
 
+        });
+
+        it('should create a did document', async () => {
+
+        });
     });
-
-    it('should create a did document', async () => {
-
-    });
-  });
 });
