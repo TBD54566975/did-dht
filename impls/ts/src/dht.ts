6bc701033dbfa996cd4559ff6e2290dc2d3f782e
gabe <gcohen@squareup.com>
2023-10-05 17:03:09 -0700

    working

--- impls/ts/src/dht.ts
+++ impls/ts/src/dht.ts
@@ -1,19 +1,19 @@
-import DHT from 'bittorrent-dht'
-import {Web5Crypto} from "@web5/crypto";
-import {Codec} from "./codec.js";
-import ed from 'bittorrent-dht-sodium'
-import {DidDocument} from "@web5/dids";
+import DHT from 'bittorrent-dht';
+import {Web5Crypto} from '@web5/crypto';
+import {Codec} from './codec.js';
+import ed from 'bittorrent-dht-sodium';
+import {DidDocument} from '@web5/dids';
 
 const DEFAULT_BOOTSTRAP = [
-    'router.magnets.im:6881',
-    'router.bittorrent.com:6881',
-    'router.utorrent.com:6881',
-    'dht.transmissionbt.com:6881',
-    'router.nuh.dev:6881'
+  'router.magnets.im:6881',
+  'router.bittorrent.com:6881',
+  'router.utorrent.com:6881',
+  'dht.transmissionbt.com:6881',
+  'router.nuh.dev:6881'
 ].map(addr => {
-    const [host, port] = addr.split(':')
-    return {host, port: Number(port)}
-})
+  const [host, port] = addr.split(':');
+  return {host, port: Number(port)};
+});
 
 export type PutRequest = {
     // sequence number of the request
@@ -27,70 +27,70 @@ export type PutRequest = {
 };
 
 export class DidDht {
-    private dht: DHT;
+  private dht: DHT;
 
-    constructor() {
-        this.dht = new DHT({bootstrap: DEFAULT_BOOTSTRAP, verify: ed.verify});
+  constructor() {
+    this.dht = new DHT({bootstrap: DEFAULT_BOOTSTRAP, verify: ed.verify});
 
-        this.dht.listen(20000, () => {
-            console.log('DHT is listening on port 20000');
-        });
-    }
+    this.dht.listen(0, () => {
+      console.debug('DHT is listening...');
+    });
+  }
 
-    public async createPutDidRequest(keypair: Web5Crypto.CryptoKeyPair, did: DidDocument): Promise<PutRequest> {
-        const seq = Math.ceil(Date.now() / 1000);
-        const records: string[][] = [['did', JSON.stringify(did)]]
-        const v = await new Codec().compress(records);
-        return {
-            seq: seq,
-            v: Buffer.from(v),
-            k: Buffer.from(keypair.publicKey.material),
-            sk: Buffer.concat([keypair.privateKey.material, keypair.publicKey.material])
-        };
-    }
+  public async createPutDidRequest(keypair: Web5Crypto.CryptoKeyPair, did: DidDocument): Promise<PutRequest> {
+    const seq = Math.ceil(Date.now() / 1000);
+    const records: string[][] = [['did', JSON.stringify(did)]];
+    const v = await new Codec().compress(records);
+    return {
+      seq : seq,
+      v   : Buffer.from(v),
+      k   : Buffer.from(keypair.publicKey.material),
+      sk  : Buffer.concat([keypair.privateKey.material, keypair.publicKey.material])
+    };
+  }
 
-    public async put(request: PutRequest): Promise<string> {
-        const opts = {
-            k: request.k,
-            v: request.v,
-            seq: request.seq,
-            sign: function (buf: Buffer) {
-                return ed.sign(buf, request.sk)
-            }
+  public async put(request: PutRequest): Promise<string> {
+    const opts = {
+      k    : request.k,
+      v    : request.v,
+      seq  : request.seq,
+      sign : function (buf: Buffer) {
+        return ed.sign(buf, request.sk);
+      }
+    };
+    return new Promise((resolve, reject) => {
+      this.dht.put(opts, (err, hash) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(hash.toString('hex'));
         }
-        return new Promise((resolve, reject) => {
-            this.dht.put(opts, (err, hash) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(hash.toString('hex'));
-                }
-            });
-        });
-    }
+      });
+    });
+  }
 
-    public async parseGetDidResponse(response: Buffer): Promise<DidDocument> {
-        const records = await new Codec().decompress(response);
-        const didRecord = records.find(record => record[0] === 'did');
-        if (!didRecord) {
-            throw new Error('No DID record found');
+  public async parseGetDidResponse(response: Buffer): Promise<DidDocument> {
+    const records = await new Codec().decompress(response);
+    const didRecord = records.find(record => record[0] === 'did');
+    if (!didRecord) {
+      throw new Error('No DID record found');
+    }
+    return JSON.parse(didRecord[1]);
+  }
+
+  public async get(keyHash: string): Promise<Buffer> {
+    return new Promise((resolve, reject) => {
+      this.dht.get(keyHash, (err, res) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(res.v);
         }
-        return JSON.parse(didRecord[1]);
-    }
+      });
+    });
+  }
 
-    public async get(keyHash: string): Promise<Buffer> {
-        return new Promise((resolve, reject) => {
-            this.dht.get(keyHash, (err, res) => {
-                if (err) {
-                    reject(err);
-                } else {
-                    resolve(res.v);
-                }
-            });
-        });
-    }
-
-    public destroy(): void {
-        this.dht.destroy();
-    }
+  public destroy(): void {
+    this.dht.destroy();
+  }
 }
--- impls/ts/tests/dht.spec.ts
+++ impls/ts/tests/dht.spec.ts
@@ -1,30 +1,30 @@
 import {expect} from 'chai';
-import {DidDht} from "../src/dht.js";
-import {DidDhtMethod} from "../src/did-dht.js";
-import {Jose} from "@web5/crypto";
-describe('DHT', async function () {
-    this.timeout(100000); // 15 seconds
+import {DidDht} from '../src/dht.js';
+import {DidDhtMethod} from '../src/did-dht.js';
+import {Jose} from '@web5/crypto';
+describe('DHT', function () {
+  this.timeout(20000); // 20 seconds
 
-    const dht = new DidDht();
-    after(() => {
-        dht.destroy();
-    });
+  const dht = new DidDht();
+  after(() => {
+    dht.destroy();
+  });
 
-    it('should put and get data from DHT', async () => {
-        const {did, keySet} = await DidDhtMethod.create();
-        const publicCryptoKey = await Jose.jwkToCryptoKey({key: keySet.identityKey.publicKeyJwk});
-        const privateCryptoKey = await Jose.jwkToCryptoKey({key: keySet.identityKey.privateKeyJwk});
+  it('should put and get data from DHT', async () => {
+    const {did, keySet} = await DidDhtMethod.create();
+    const publicCryptoKey = await Jose.jwkToCryptoKey({key: keySet.identityKey.publicKeyJwk});
+    const privateCryptoKey = await Jose.jwkToCryptoKey({key: keySet.identityKey.privateKeyJwk});
 
-        const request = await dht.createPutDidRequest({
-            publicKey: publicCryptoKey,
-            privateKey: privateCryptoKey
-        }, did);
+    const request = await dht.createPutDidRequest({
+      publicKey  : publicCryptoKey,
+      privateKey : privateCryptoKey
+    }, did);
 
-        const hash = await dht.put(request);
+    const hash = await dht.put(request);
 
-        const retrievedValue = await dht.get(hash);
+    const retrievedValue = await dht.get(hash);
 
-        const gotDid = await dht.parseGetDidResponse(retrievedValue);
-        expect(gotDid).to.deep.equal(did);
-    });
+    const gotDid = await dht.parseGetDidResponse(retrievedValue);
+    expect(gotDid).to.deep.equal(did);
+  });
 });
