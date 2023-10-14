7f2587f13af7a7b6429f6bb2bf792862df870dd1
gabe <gcohen@squareup.com>
2023-10-14 13:28:18 -0700

    did generation

--- impl/go.mod
+++ impl/go.mod
@@ -37,13 +37,21 @@ require (
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
 	github.com/edsrzf/mmap-go v1.1.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.13.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/huandu/xstrings v1.3.2 // indirect
 	github.com/hyperledger/aries-framework-go v0.3.1 // indirect
 	github.com/hyperledger/aries-framework-go/component/kmscrypto v0.0.0-20230427134832-0c9969493bd3 // indirect
+	github.com/hyperledger/aries-framework-go/component/log v0.0.0-20230427134832-0c9969493bd3 // indirect
+	github.com/hyperledger/aries-framework-go/component/models v0.0.0-20230501135648-a9a7ad029347 // indirect
+	github.com/hyperledger/aries-framework-go/spi v0.0.0-20230427134832-0c9969493bd3 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/kilic/bls12-381 v0.1.1-0.20210503002446-7b7597926c69 // indirect
+	github.com/leodido/go-urn v1.2.3 // indirect
 	github.com/lestrrat-go/blackmagic v1.0.1 // indirect
 	github.com/lestrrat-go/httpcc v1.0.1 // indirect
 	github.com/lestrrat-go/httprc v1.0.4 // indirect
@@ -53,9 +61,16 @@ require (
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/miekg/dns v1.1.56 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/multiformats/go-base32 v0.1.0 // indirect
+	github.com/multiformats/go-base36 v0.1.0 // indirect
+	github.com/multiformats/go-multibase v0.2.0 // indirect
+	github.com/multiformats/go-multicodec v0.9.0 // indirect
+	github.com/multiformats/go-varint v0.0.7 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
+	github.com/piprate/json-gold v0.5.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/pquerna/cachecontrol v0.1.0 // indirect
 	github.com/rs/dnscache v0.0.0-20211102005908-e0241e321417 // indirect
 	github.com/sagikazarmark/locafero v0.3.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
--- impl/internal/did/did.go
+++ impl/internal/did/did.go
@@ -1,10 +1,17 @@
 package did
 
 import (
+	"crypto/ed25519"
 	"fmt"
 	"strings"
 
+	"github.com/TBD54566975/ssi-sdk/crypto"
+	"github.com/TBD54566975/ssi-sdk/crypto/jwx"
+	"github.com/TBD54566975/ssi-sdk/cryptosuite"
 	"github.com/TBD54566975/ssi-sdk/did"
+	"github.com/TBD54566975/ssi-sdk/did/ion"
+	"github.com/miekg/dns"
+	"github.com/tv42/zbase32"
 )
 
 type (
@@ -37,10 +44,137 @@ func (DHT) Method() did.Method {
 	return DHTMethod
 }
 
-func GenerateDIDDHT() {
-
+type CreateDIDDHTOpts struct {
+	// VerificationMethods is a list of verification methods to include in the DID Document
+	// Cannot contain id #0 which is reserved for the identity key
+	VerificationMethods []VerificationMethods
+	// Services is a list of services to include in the DID Document
+	Services []did.Service
 }
 
-func CreateDIDDHTDID(pubKey []byte) (string, error) {
-	return "", nil
+type VerificationMethods struct {
+	VerificationMethod did.VerificationMethod `json:"verificationMethod"`
+	Purposes           []ion.PublicKeyPurpose `json:"purposes"`
+}
+
+// GenerateDIDDHT generates a did:dht identifier given a set of options
+func GenerateDIDDHT(opts CreateDIDDHTOpts) (ed25519.PrivateKey, *did.Document, error) {
+	// generate the identity key
+	pubKey, privKey, err := crypto.GenerateEd25519Key()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	doc, err := CreateDIDDHTDID(pubKey, opts)
+	return privKey, doc, err
+}
+
+// CreateDIDDHTDID creates a did:dht identifier for the given ed25519 public key
+func CreateDIDDHTDID(pubKey ed25519.PublicKey, opts CreateDIDDHTOpts) (*did.Document, error) {
+	// generate the did:dht identifier
+	id := GetDIDDHTIdentifier(pubKey)
+
+	// validate opts and build verification methods, key purposes, and services
+	var vms []did.VerificationMethod
+	var authentication []did.VerificationMethodSet
+	var assertionMethod []did.VerificationMethodSet
+	var keyAgreement []did.VerificationMethodSet
+	var capabilityInvocation []did.VerificationMethodSet
+	var capabilityDelegation []did.VerificationMethodSet
+	if len(opts.VerificationMethods) > 0 {
+		seenIDs := make(map[string]bool)
+		for _, vm := range opts.VerificationMethods {
+			if vm.VerificationMethod.ID == "#0" || vm.VerificationMethod.ID == "0" {
+				return nil, fmt.Errorf("verification method id #0 is reserved for the identity key")
+			}
+			if seenIDs[vm.VerificationMethod.ID] {
+				return nil, fmt.Errorf("verification method id %s is not unique", vm.VerificationMethod.ID)
+			}
+			if vm.VerificationMethod.Type != cryptosuite.JSONWebKey2020Type {
+				return nil, fmt.Errorf("verification method type %s is not supported", vm.VerificationMethod.Type)
+			}
+			if vm.VerificationMethod.PublicKeyJWK == nil {
+				return nil, fmt.Errorf("verification method public key jwk is required")
+			}
+
+			// mark as seen
+			seenIDs[vm.VerificationMethod.ID] = true
+
+			// update ID and controller in place
+			vm.VerificationMethod.ID = id + "#" + vm.VerificationMethod.ID
+			vm.VerificationMethod.Controller = id
+			vms = append(vms, vm.VerificationMethod)
+
+			// add purposes
+			for _, purpose := range vm.Purposes {
+				switch purpose {
+				case ion.Authentication:
+					authentication = append(authentication, vm.VerificationMethod.ID)
+				case ion.AssertionMethod:
+					assertionMethod = append(assertionMethod, vm.VerificationMethod.ID)
+				case ion.KeyAgreement:
+					keyAgreement = append(keyAgreement, vm.VerificationMethod.ID)
+				case ion.CapabilityInvocation:
+					capabilityInvocation = append(capabilityInvocation, vm.VerificationMethod.ID)
+				case ion.CapabilityDelegation:
+					capabilityDelegation = append(capabilityDelegation, vm.VerificationMethod.ID)
+				default:
+					return nil, fmt.Errorf("unknown key purpose: %s:%s", vm.VerificationMethod.ID, purpose)
+				}
+			}
+		}
+	}
+	if len(opts.Services) > 0 {
+		seenIDs := make(map[string]bool)
+		for _, s := range opts.Services {
+			if seenIDs[s.ID] {
+				return nil, fmt.Errorf("service id %s is not unique", s.ID)
+			}
+
+			// mark as seen
+			seenIDs[s.ID] = true
+
+			// update ID in place
+			s.ID = id + "#" + s.ID
+		}
+	}
+
+	// create the did document
+	key0JWK, err := jwx.PublicKeyToPublicKeyJWK("0", pubKey)
+	if err != nil {
+		return nil, err
+	}
+	vm0 := did.VerificationMethod{
+		ID:           id + "#0",
+		Type:         cryptosuite.JSONWebKey2020Type,
+		Controller:   id,
+		PublicKeyJWK: key0JWK,
+	}
+	return &did.Document{
+		Context:              []string{did.KnownDIDContext},
+		ID:                   id,
+		VerificationMethod:   append([]did.VerificationMethod{vm0}, vms...),
+		Services:             opts.Services,
+		Authentication:       authentication,
+		AssertionMethod:      assertionMethod,
+		KeyAgreement:         keyAgreement,
+		CapabilityInvocation: capabilityInvocation,
+		CapabilityDelegation: capabilityDelegation,
+	}, nil
+}
+
+// GetDIDDHTIdentifier returns the did:dht identifier for the given public key
+func GetDIDDHTIdentifier(pubKey []byte) string {
+	return strings.Join([]string{Prefix, zbase32.EncodeToString(pubKey)}, ":")
+}
+
+func (d DHT) ToDNSPacket(did did.Document) ([]dns.RR, error) {
+	if d.String() != did.ID {
+		return nil, fmt.Errorf("did and dht id mismatch")
+	}
+	return nil, nil
+}
+
+func (d DHT) FromDNSPacket() (*did.Document, error) {
+	return nil, nil
 }
