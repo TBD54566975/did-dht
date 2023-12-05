9194561f88646b11ef7a8bfd073d1153cd59568c
gabe <gcohen@squareup.com>
2023-12-05 13:15:46 -0800

    dont run 1000 times

--- impl/internal/did/client_test.go
+++ impl/internal/did/client_test.go
@@ -15,27 +15,24 @@ func TestClient(t *testing.T) {
 	require.NoError(t, err)
 	start := time.Now()
 
-	for i := 0; i < 1000; i++ {
-		sk, doc, err := GenerateDIDDHT(CreateDIDDHTOpts{})
-		require.NoError(t, err)
-		require.NotEmpty(t, doc)
+	sk, doc, err := GenerateDIDDHT(CreateDIDDHTOpts{})
+	require.NoError(t, err)
+	require.NotEmpty(t, doc)
 
-		packet, err := DHT(doc.ID).ToDNSPacket(*doc, nil)
-		assert.NoError(t, err)
-		assert.NotEmpty(t, packet)
+	packet, err := DHT(doc.ID).ToDNSPacket(*doc, nil)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, packet)
 
-		bep44Put, err := dht.CreatePKARRPublishRequest(sk, *packet)
-		assert.NoError(t, err)
-		assert.NotEmpty(t, bep44Put)
+	bep44Put, err := dht.CreatePKARRPublishRequest(sk, *packet)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, bep44Put)
 
-		err = client.PutDocument(doc.ID, *bep44Put)
-		assert.NoError(t, err)
+	err = client.PutDocument(doc.ID, *bep44Put)
+	assert.NoError(t, err)
 
-		gotDID, _, err := client.GetDIDDocument(doc.ID)
-		assert.NoError(t, err)
-		assert.EqualValues(t, doc, gotDID)
-
-	}
+	gotDID, _, err := client.GetDIDDocument(doc.ID)
+	assert.NoError(t, err)
+	assert.EqualValues(t, doc, gotDID)
 
 	since := time.Since(start)
 	t.Logf("time to put and get: %s", since)
