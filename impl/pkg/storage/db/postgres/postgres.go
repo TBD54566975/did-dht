dd92b9a7f5721381488d97e28db3ec738d315377
finn <fherzfeld@tbd.email>
2024-01-19 14:41:20 -0800

    implement postgres functions

--- impl/pkg/storage/db/postgres/postgres.go
+++ impl/pkg/storage/db/postgres/postgres.go
@@ -4,10 +4,9 @@ import (
 	"context"
 	"database/sql"
 	"embed"
-	"errors"
 
 	"github.com/TBD54566975/did-dht-method/pkg/storage/pkarr"
-	_ "github.com/jackc/pgx/v5"
+	pgx "github.com/jackc/pgx/v5"
 	goose "github.com/pressly/goose/v3"
 )
 
@@ -44,14 +43,75 @@ func (p postgres) migrate() error {
 	return nil
 }
 
+func (p postgres) connect(ctx context.Context) (*Queries, *pgx.Conn, error) {
+	conn, err := pgx.Connect(ctx, string(p))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return New(conn), conn, nil
+}
+
 func (p postgres) WriteRecord(ctx context.Context, record pkarr.PkarrRecord) error {
-	return errors.New("not yet implemented")
+	queries, db, err := p.connect(ctx)
+	if err != nil {
+		return err
+	}
+	defer db.Close(ctx)
+
+	err = queries.WriteRecord(ctx, WriteRecordParams{
+		Key:   record.K,
+		Value: record.V,
+		Sig:   record.Sig,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (p postgres) ReadRecord(ctx context.Context, id string) (*pkarr.PkarrRecord, error) {
-	return nil, errors.New("not yet implemented")
+	queries, db, err := p.connect(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close(ctx)
+
+	record, err := queries.ReadRecord(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pkarr.PkarrRecord{
+		K:   record.Key,
+		V:   record.Value,
+		Sig: record.Sig,
+		Seq: int64(record.ID),
+	}, nil
 }
 
 func (p postgres) ListRecords(ctx context.Context) ([]pkarr.PkarrRecord, error) {
-	return nil, errors.New("not yet implemented")
+	queries, db, err := p.connect(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close(ctx)
+
+	rows, err := queries.ListRecords(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var records []pkarr.PkarrRecord
+	for _, row := range rows {
+		records = append(records, pkarr.PkarrRecord{
+			K:   row.Key,
+			V:   row.Value,
+			Sig: row.Sig,
+			Seq: int64(row.ID),
+		})
+	}
+
+	return records, nil
 }
